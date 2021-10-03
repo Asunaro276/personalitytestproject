cbd2795b8b587ea7973644637fb05c758c1d99df
asunaro276 <ryuhei276276@gmail.com>
2021-10-03 18:54:55 +0900

    変更

--- works/urls.py
+++ works/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import QuestionnaireView, ResultView
+from .views import QuestionnaireView, ResultView, my_customized_server_error
 
 
 urlpatterns = [
     path('', QuestionnaireView.as_view(), name='questionnaire'),
     path('result/', ResultView.as_view(), name='result'),
 ]
+
+handler500 = my_customized_server_error
--- works/views.py
+++ works/views.py
@@ -69,3 +69,10 @@ def get_image():
     graph = graph.decode('utf-8')
     buffer.close()
     return graph
+
+
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import HttpResponseServerError
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    return HttpResponseServerError('<h1>Server Error (500)だよー</h1>')
