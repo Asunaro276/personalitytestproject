a126f129725bb3292f341b58b9c82b4954f1dc7d
asunaro276 <ryuhei276276@gmail.com>
2021-10-03 20:43:43 +0900

    BASE_DIRを変更

--- personalitytestproject/settings/base.py
+++ personalitytestproject/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PROJECT_NAME = os.path.basename(BASE_DIR)
 
